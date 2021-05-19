7e9314add8a019d891852b3e3f6f4e1fe756b463
Michael Haines <michael.haines@santacruzbicycles.com>
2021-05-19 11:30:19 -0600

    unnecessary comments

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -45,19 +45,7 @@ function Post({ id }) {
   );
 }
 
-// This function gets called at build time
 export async function getStaticPaths() {
-  // Call an external API endpoint to get posts
-  // const res = await fetch('https://.../posts')
-  // const posts = await res.json()
-
-  // Get the paths we want to pre-render based on posts
-  // const paths = posts.map((post) => ({
-  //   params: { id: post.id },
-  // }))
-
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
   return { paths: [], fallback: "blocking" };
 }
 
